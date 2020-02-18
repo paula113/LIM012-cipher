f452781cfada9e268a7096b4030a4a0c46bae8c5
pierina113 <52730693+pierina113@users.noreply.github.com>
2020-02-18 12:43:57 -0500

    funcion encode modificada

--- src/cipher.js
+++ src/cipher.js
@@ -1,9 +1,30 @@
 
 const cipher = {
-    encode(string, offset){
-      let msg = string;
-      return msg;
-    }
+  encode: (string, offset) => {
+    //mensaje recibido
+    let respuesta = '';
+    let asciiP = 0;
+    let formula = 0;
+    string.toUpperCase().split('').forEach((ele) => {
+      asciiP = ele.charCodeAt(0);
+      formula = (asciiP - 65 + parseInt(offset)) % 26 + 65;
+      respuesta += String.fromCharCode(formula);
+    });
+    return respuesta;
+  },
 
-}
+  decode: (string, offset) => {
+    let respuestaD = '';
+    let asciiPD = 0;
+    let formulaD = 0;
+    let spacioD= " ";
+    string.toUpperCase().split('').forEach((ele) => {
+      asciiPD = ele.charCodeAt(0);
+      formulaD = (asciiPD - 65 + parseInt(offset)) % 26 + 65;
+      respuestaD += String.fromCharCode(formulaD);
+    });
+    return respuestaD;
+  },
+};
+
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,46 +1,23 @@
 import cipher from './cipher.js';
 //DOM
-
-encriptar.addEventListener("click", function E(mensaje, number){
+const encriptar = document.getElementById('encriptar');
+encriptar.addEventListener("click", (mensaje, number) => {
   mensaje = document.getElementById('mensaje').value;
   number = document.getElementById('number').value;
-  let msg = (function(str, num) {
-    let result = "";
-    str = mensaje.toUpperCase();
-    num = number;
-    for (var i = 0; i < str.length; i++) {
-      let asciiP = str[i].charCodeAt();
-        let alphaTotal = asciiP - 65 + num;
-        let r = alphaTotal % 26;
-        result += String.fromCharCode( r + 65);
-    }
 
-  return result;
+  //  let msg = (function(str, num)
+  document.getElementById("msg").innerHTML = cipher.encode(mensaje, number);
+});
 
-})();
-
-  document.getElementById("msg").innerHTML = msg;
- });
-
-desencriptar.addEventListener("click", function D(m, n){
+const desencriptar = document.getElementById('desencriptar');
+desencriptar.addEventListener("click", (m, n) =>{
   m = document.getElementById('mensaje').value;
   n = document.getElementById('number').value;
-  let msgD =  (function(msg, numD) {
-    msg = m;
-    numD = n;
-    let solved = '';
-    for (var i = 0; i < m.length; i++) {
-     let reverse = msg[i].charCodeAt();
-    }
-    return solved;
+  document.getElementById("msg").innerHTML = cipher.decode(m, n);
+});
 
-})();
-
-   document.getElementById("msg").innerHTML = msgD;
-  });
-
-limpiar.addEventListener('click', (event) =>{
+const limpiar = document.getElementById('limpiar');
+limpiar.addEventListener('click', () => {
   event.preventDefault();
   location.reload();
-
 });
