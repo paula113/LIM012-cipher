8d89a610d8adcc14fff31c7151b3f30475a6824d
paula113 <52730693+pierina113@users.noreply.github.com>
2020-02-26 11:52:36 -0500

    intercartive design

--- src/cipher.js
+++ src/cipher.js
@@ -1,15 +1,27 @@
 
 const cipher = {
   encode: (string, offset) => {
-    // mensaje recibido
     var respuesta = '';
     let asciiP = 0;
     let formula = 0;
     string.split('').forEach((ele) => {
-      ele.toUpperCase();
+      // ele.toUpperCase(); 
       asciiP = ele.charCodeAt(0);
-      formula = (asciiP - 65 + parseInt(offset)) % 26 + 65;
-      formula > 90 ? formula = formula - 26 : formula;
+      if (asciiP >= 65 && asciiP <= 90) {
+        formula = (asciiP - 65 + parseInt(offset)) % 26 + 65;
+      } else if (asciiP === 32) {
+        formula = (asciiP - 65) + 65;
+      } else if (asciiP > 90) {
+        formula = formula - 26;
+      }
+      // minusculas
+      if (asciiP >= 97 && asciiP <= 122) {
+        formula = (asciiP - 97 + parseInt(offset)) % 26 + 97;
+      } else if (asciiP === 32) {
+        formula = (asciiP - 97) + 97;
+      } else if (asciiP > 122) {
+        formula = formula - 26;
+      }
       respuesta += String.fromCharCode(formula);
     });
     return respuesta;
@@ -20,14 +32,28 @@ const cipher = {
     let asciiPD = 0;
     let formulaD = 0;
     string.split('').forEach((ele) => {
-      ele.toUpperCase();
       asciiPD = ele.charCodeAt(0);
-      formulaD = ((asciiPD - 65) - (parseInt(offset) % 26)) + 65;
-      formulaD < 65 ? formulaD = formulaD + 26 : formulaD;
+      if (asciiPD >= 65 && asciiPD <= 90) {
+        formulaD = ((asciiPD - 65) - (parseInt(offset) % 26)) + 65;
+        if (formulaD < 65) {
+          formulaD = formulaD + 26;
+        } else if (asciiPD === 32) {
+          formulaD = (asciiPD - 65) + 65;
+        }
+      } else {
+        // minusculas
+        if (asciiPD >= 97 && asciiPD <= 122) {
+          formulaD = ((asciiPD - 97) - (parseInt(offset) % 26)) + 97;
+        }
+        if (formulaD < 97 && formulaD > 90) {
+          formulaD = formulaD + 26;
+        } else if (asciiPD === 32) {
+          formulaD = (asciiPD - 97) + 97;
+        }
+      }
       respuestaD += String.fromCharCode(formulaD);
     });
     return respuestaD;
   },
 };
-
 export default cipher;
